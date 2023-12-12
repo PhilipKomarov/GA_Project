4ba629162d30b5b6a6a3d99ada241aa33016bd21
John Doe <johndoe@example.com>
2023-12-12 19:24:04 +1100

    wip

---
+++ project.js
@@ -0,0 +1,40 @@
+document.getElementById('menu-1-button').addEventListener('click', myFunction1);
+document.getElementById('menu-2-button').addEventListener('click', myFunction2);
+document.getElementById('menu-3-button').addEventListener('click', myFunction3);
+document.getElementById('menu-4-button').addEventListener('click', myFunction4);
+
+function myFunction1()
+{
+  document.getElementById('menu-1').classList.add('show');
+  document.getElementById('menu-2').classList.remove('show');
+  document.getElementById('menu-3').classList.remove('show');
+  document.getElementById('menu-4').classList.remove('show');
+}
+
+function myFunction2()
+{
+  document.getElementById('menu-1').classList.remove('show');
+  document.getElementById('menu-2').classList.add('show');
+  document.getElementById('menu-3').classList.remove('show');
+  document.getElementById('menu-4').classList.remove('show');
+}
+
+function myFunction3()
+{
+  document.getElementById('menu-1').classList.remove('show');
+  document.getElementById('menu-2').classList.remove('show');
+  document.getElementById('menu-3').classList.add('show');
+  document.getElementById('menu-4').classList.remove('show');
+}
+
+function myFunction4()
+{
+  document.getElementById('menu-1').classList.remove('show');
+  document.getElementById('menu-2').classList.remove('show');
+  document.getElementById('menu-3').classList.remove('show');
+  document.getElementById('menu-4').classList.add('show');
+}
+
+function myFunction() {
+  alert("Very nice!");
+}
